8cfc235e30aba33b77882d593b66269716223aff
Alan Krajina <alankrajina88@gmail.com>
2019-05-12 16:41:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ food = ["crackers", "gouda", "thyme"]
 
 def find_the_cheese(food)
   array= ["cheddar", "gouda", "camembert"]
-  food.include? ("cheddar" or "gouda" or "camembert")
+  food.include? (array)
 end
 
 find_the_cheese(food)
